368456f16c83117beead56da4826f9f88b5a25b0
maverick <127213451+DreamFlightStudios@users.noreply.github.com>
2023-10-05 15:32:37 +0300

    ячсячс

--- Flappybird3D/Assets/Scripts/RoadSpawner.cs
+++ Flappybird3D/Assets/Scripts/RoadSpawner.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 public class RoadSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _roads;
+    [SerializeField] private GameObject _pipe;
+    private void Start()
+    {
+        InvokeRepeating("PipeSpawner", 2, 1);
+    }
     public void Spawner()
     {
         GameObject _road = Instantiate(_roads[Random.Range(0, _roads.Count)], transform.position, Quaternion.identity);
+    }
+    private void PipeSpawner()
+    {
+        Vector3 height = new Vector3(transform.position.x, Random.Range(3, 13), transform.position.z);
+        Instantiate(_pipe, height, Quaternion.identity);
     }
 }
